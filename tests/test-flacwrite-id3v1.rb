4bcc8e601667c71bb43f9aa1b1f96c36cc611574
Diego Pettenò <flameeyes@gentoo.org>
2006-05-14 17:25:42 +0200

    Add test for writing on ID3v1 tags of FLAC files. Does not pass. Might be related to the generic tag's fault.

---
+++ tests/test-flacwrite-id3v1.rb
@@ -0,0 +1,36 @@
+# Test n. 7, simple test of getting data out of a flac file
+
+require "rubytagpp"
+require "converters"
+
+cvt = FlacConverter.new
+
+savefile = TagLib::FLAC::File.new(cvt.path)
+exit -2 unless savefile.open?
+
+exit -3 unless savefile.ID3v1Tag(true)
+
+savefile.ID3v1Tag.title = Converter::TEST_TITLE
+savefile.ID3v1Tag.album = Converter::TEST_ALBUM
+savefile.ID3v1Tag.artist = Converter::TEST_ARTIST
+savefile.ID3v1Tag.comment = Converter::TEST_COMMENT
+
+savefile.save
+
+readfile = TagLib::FLAC::File.new(cvt.path)
+exit -4 unless readfile.open?
+
+puts %@
+	Title: "#{readfile.ID3v1Tag.title}" (should be "#{Converter::TEST_TITLE}")
+	Album: "#{readfile.ID3v1Tag.album}" (should be "#{Converter::TEST_ALBUM}")
+	Artist: "#{readfile.ID3v1Tag.artist}" (should be "#{Converter::TEST_ARTIST}")
+	Comment: "#{readfile.ID3v1Tag.comment}" (should be "#{Converter::TEST_COMMENT}")
+@
+
+exit -5 unless \
+	readfile.ID3v1Tag.title == Converter::TEST_TITLE and \
+	readfile.ID3v1Tag.album == Converter::TEST_ALBUM and \
+	readfile.ID3v1Tag.artist == Converter::TEST_ARTIST and \
+	readfile.ID3v1Tag.comment == Converter::TEST_COMMENT
+
+exit 0
