4844b5735b25e2efe80de38ffda7dc9b7a21b963
Diego Pettenò <flameeyes@gentoo.org>
2006-05-15 00:53:30 +0200

    Add tests for writing on Ogg Vorbis files (both via attributes and fields).

---
+++ tests/test-vorbiswrite-fields.rb
@@ -0,0 +1,41 @@
+# Simple test of saving data from and reading it to an Ogg Vorbis
+# (via fields)
+
+require "rubytagpp"
+require "converters"
+
+cvt = VorbisConverter.new(false)
+
+savefile = TagLib::Ogg::Vorbis::File.new(cvt.path)
+exit -3 unless savefile.open? or savefile.read_only?
+
+savefile.tag.addField("TITLE", Converter::TEST_TITLE)
+savefile.tag.addField("ALBUM", Converter::TEST_ALBUM)
+savefile.tag.addField("ARTIST", Converter::TEST_ARTIST)
+savefile.tag.addField("COMMENT", Converter::TEST_COMMENT)
+savefile.tag.addField("TESTFIELD", Converter::TEST_TESTFIELD)
+
+if not savefile.save
+   puts "Error while saving the file."
+   exit -2
+end
+
+readfile = TagLib::Ogg::Vorbis::File.new(cvt.path)
+exit -4 unless readfile.open?
+
+puts %@
+	Title: "#{readfile.tag.fieldListMap["TITLE"][0]}" (should be "#{Converter::TEST_TITLE}")
+	Album: "#{readfile.tag.fieldListMap["ALBUM"][0]}" (should be "#{Converter::TEST_ALBUM}")
+	Artist: "#{readfile.tag.fieldListMap["ARTIST"][0]}" (should be "#{Converter::TEST_ARTIST}")
+	Comment: "#{readfile.tag.fieldListMap["COMMENT"][0]}" (should be "#{Converter::TEST_COMMENT}")
+	Test Field: "#{readfile.tag.fieldListMap["TESTFIELD"][0]}" (should be "#{Converter::TEST_TESTFIELD}")
+@
+
+exit -4 unless \
+   readfile.tag.fieldListMap["TITLE"][0] == Converter::TEST_TITLE and \
+   readfile.tag.fieldListMap["ALBUM"][0] == Converter::TEST_ALBUM and \
+   readfile.tag.fieldListMap["ARTIST"][0] == Converter::TEST_ARTIST and \
+   readfile.tag.fieldListMap["COMMENT"][0] == Converter::TEST_COMMENT and \
+   readfile.tag.fieldListMap["TESTFIELD"][0] == Converter::TEST_TESTFIELD
+
+exit 0
---
+++ tests/test-vorbiswrite.rb
@@ -0,0 +1,38 @@
+# Simple test of saving data from and reading it to an Ogg Vorbis
+# (via attributes)
+
+require "rubytagpp"
+require "converters"
+
+cvt = VorbisConverter.new(false)
+
+savefile = TagLib::Ogg::Vorbis::File.new(cvt.path)
+exit -3 unless savefile.open? or savefile.read_only?
+
+savefile.tag.title = Converter::TEST_TITLE
+savefile.tag.album = Converter::TEST_ALBUM
+savefile.tag.artist = Converter::TEST_ARTIST
+savefile.tag.track = Converter::TEST_TRACK
+
+if not savefile.save
+   puts "Error while saving the file."
+   exit -2
+end
+
+readfile = TagLib::Ogg::Vorbis::File.new(cvt.path)
+exit -4 unless readfile.open?
+
+puts %@
+	Title: "#{readfile.tag.title}" (should be "#{Converter::TEST_TITLE}")
+	Album: "#{readfile.tag.album}" (should be "#{Converter::TEST_ALBUM}")
+	Artist: "#{readfile.tag.artist}" (should be "#{Converter::TEST_ARTIST}")
+	Track: "#{readfile.tag.track}" (should be "#{Converter::TEST_TRACK}")
+@
+
+exit -5 unless \
+	readfile.tag.title == Converter::TEST_TITLE and \
+	readfile.tag.album == Converter::TEST_ALBUM and \
+	readfile.tag.artist == Converter::TEST_ARTIST and \
+	readfile.tag.track == Converter::TEST_TRACK
+
+exit 0
