69a901e37de5711e51b75a2ba6fc59db556a7dff
Diego Pettenò <flameeyes@gentoo.org>
2006-05-14 04:08:21 +0200

    Fix position of kate modeline.

--- generator/class.rb
+++ generator/class.rb
@@ -14,8 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# kate: encoding UTF-8; remove-trailing-space on; replace-trailing-space-save on; space-indent on; indent-width 3;
 
 class Class
    attr_reader :ns, :name, :children
@@ -190,3 +188,5 @@ rb_define_alloc_func(c#{varname}, #{varname}_alloc);
       @
    end
 end
+
+# kate: encoding UTF-8; remove-trailing-space on; replace-trailing-space-save on; space-indent on; indent-width 3;
