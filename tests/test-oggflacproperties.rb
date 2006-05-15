184f3802e467b09e2d8111c97bc76bd240972486
Diego Pettenò <flameeyes@gentoo.org>
2006-05-15 18:27:16 +0200

    Add test for OggFlac's properties. Missing TagLib::FLAC::Properties class to pass.

---
+++ tests/test-oggflacproperties.rb
@@ -0,0 +1,27 @@
+# This test will check that the Ogg Vorbis properties read from the file 
+# correspond to the ones asked to oggenc.
+
+require "rubytagpp"
+require "converters"
+
+cvt = OggFlacConverter.new(false)
+
+file = TagLib::Ogg::FLAC::File.new(cvt.path)
+exit -3 unless file.open?
+
+puts "File audio properties are #{file.audio_properties.class} (should be TagLib::FLAC::Properties)"
+exit -4 if file.audio_properties and not file.audio_properties.is_a?(TagLib::FLAC::Properties)
+
+puts %@
+	Sample Rate: #{file.audio_properties.sample_rate} (should be #{Converter::SAMPLE_RATE})
+	Channels: #{file.audio_properties.channels} (should be #{Converter::CHANNELS})
+	Length: #{file.audio_properties.length} (should be #{Converter::LENGTH})
+	Sample Width: #{file.audio_properties.sample_width} (just information)
+@
+
+# oggenc takes bitrates in kbps, while taglib returns them in bps, take this into account!
+exit -5 if file.audio_properties.sample_rate != Converter::SAMPLE_RATE or \
+	file.audio_properties.channels != Converter::CHANNELS or \
+	file.audio_properties.length != Converter::LENGTH
+
+exit 0
